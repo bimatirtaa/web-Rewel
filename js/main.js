146c69f216d985781837733554a501136d9c79ff
bimatirtaa <bima.1666@students.amikom.ac.id>
2023-02-04 23:24:56 +0700

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,261 @@
+'use strict';
+
+$(window).on('load', function() {
+	/*------------------
+		Preloder
+	--------------------*/
+	$(".loader").fadeOut();
+	$("#preloder").delay(400).fadeOut("slow");
+
+});
+
+(function($) {
+	/*------------------
+		Navigation
+	--------------------*/
+	$('.main-menu').slicknav({
+		prependTo:'.main-navbar .container',
+		closedSymbol: '<i class="flaticon-right-arrow"></i>',
+		openedSymbol: '<i class="flaticon-down-arrow"></i>'
+	});
+
+
+	/*------------------
+		ScrollBar
+	--------------------*/
+	$(".cart-table-warp, .product-thumbs").niceScroll({
+		cursorborder:"",
+		cursorcolor:"#afafaf",
+		boxzoom:false
+	});
+
+
+	/*------------------
+		Category menu
+	--------------------*/
+	$('.category-menu > li').hover( function(e) {
+		$(this).addClass('active');
+		e.preventDefault();
+	});
+	$('.category-menu').mouseleave( function(e) {
+		$('.category-menu li').removeClass('active');
+		e.preventDefault();
+	});
+
+
+	/*------------------
+		Background Set
+	--------------------*/
+
+
+	$('.set-bg').each(function() {
+		var bg = $(this).data('setbg');
+		$(this).css('background-image', 'url(' + bg + ')');
+	});
+
+
+	/*------------------
+		Hero Slider
+	--------------------*/
+	var hero_s = $(".hero-slider");
+    hero_s.owlCarousel({
+        loop: true,
+        margin: 0,
+        nav: true,
+        items: 1,
+        dots: true,
+        animateOut: 'fadeOut',
+    	animateIn: 'fadeIn',
+        navText: ['<i class="flaticon-left-arrow-1"></i>', '<i class="flaticon-right-arrow-1"></i>'],
+        smartSpeed: 1200,
+        autoHeight: false,
+        autoplay: true,
+        onInitialized: function() {
+        	var a = this.items().length;
+            $("#snh-1").html("<span>1</span><span>" + a + "</span>");
+        }
+    }).on("changed.owl.carousel", function(a) {
+        var b = --a.item.index, a = a.item.count;
+    	$("#snh-1").html("<span> "+ (1 > b ? b + a : b > a ? b - a : b) + "</span><span>" + a + "</span>");
+
+    });
+
+	hero_s.append('<div class="slider-nav-warp"><div class="slider-nav"></div></div>');
+	$(".hero-slider .owl-nav, .hero-slider .owl-dots").appendTo('.slider-nav');
+
+
+
+	/*------------------
+		Brands Slider
+	--------------------*/
+	$('.product-slider').owlCarousel({
+		loop: true,
+		nav: true,
+		dots: false,
+		margin : 30,
+		autoplay: true,
+		navText: ['<i class="flaticon-left-arrow-1"></i>', '<i class="flaticon-right-arrow-1"></i>'],
+		responsive : {
+			0 : {
+				items: 1,
+			},
+			480 : {
+				items: 2,
+			},
+			768 : {
+				items: 3,
+			},
+			1200 : {
+				items: 4,
+			}
+		}
+	});
+
+
+	/*------------------
+		Popular Services
+	--------------------*/
+	$('.popular-services-slider').owlCarousel({
+		loop: true,
+		dots: false,
+		margin : 40,
+		autoplay: true,
+		nav:true,
+		navText:['<i class="fa fa-angle-left"></i>','<i class="fa fa-angle-right"></i>'],
+		responsive : {
+			0 : {
+				items: 1,
+			},
+			768 : {
+				items: 2,
+			},
+			991: {
+				items: 3
+			}
+		}
+	});
+
+
+	/*------------------
+		Accordions
+	--------------------*/
+	$('.panel-link').on('click', function (e) {
+		$('.panel-link').removeClass('active');
+		var $this = $(this);
+		if (!$this.hasClass('active')) {
+			$this.addClass('active');
+		}
+		e.preventDefault();
+	});
+
+
+	/*-------------------
+		Range Slider
+	--------------------- */
+	var rangeSlider = $(".price-range"),
+		minamount = $("#minamount"),
+		maxamount = $("#maxamount"),
+		minPrice = rangeSlider.data('min'),
+		maxPrice = rangeSlider.data('max');
+	rangeSlider.slider({
+		range: true,
+		min: minPrice,
+		max: maxPrice,
+		values: [minPrice, maxPrice],
+		slide: function (event, ui) {
+			minamount.val('$' + ui.values[0]);
+			maxamount.val('$' + ui.values[1]);
+		}
+	});
+	minamount.val('$' + rangeSlider.slider("values", 0));
+	maxamount.val('$' + rangeSlider.slider("values", 1));
+
+
+	/*-------------------
+		Quantity change
+	--------------------- */
+    var proQty = $('.pro-qty');
+	proQty.prepend('<span class="dec qtybtn">-</span>');
+	proQty.append('<span class="inc qtybtn">+</span>');
+	proQty.on('click', '.qtybtn', function () {
+		var $button = $(this);
+		var oldValue = $button.parent().find('input').val();
+		if ($button.hasClass('inc')) {
+			var newVal = parseFloat(oldValue) + 1;
+		} else {
+			// Don't allow decrementing below zero
+			if (oldValue > 0) {
+				var newVal = parseFloat(oldValue) - 1;
+			} else {
+				newVal = 0;
+			}
+		}
+		$button.parent().find('input').val(newVal);
+	});
+
+
+
+	/*------------------
+		Single Product
+	--------------------*/
+	$('.product-thumbs-track > .pt').on('click', function(){
+		$('.product-thumbs-track .pt').removeClass('active');
+		$(this).addClass('active');
+		var imgurl = $(this).data('imgbigurl');
+		var bigImg = $('.product-big-img').attr('src');
+		if(imgurl != bigImg) {
+			$('.product-big-img').attr({src: imgurl});
+			$('.zoomImg').attr({src: imgurl});
+		}
+	});
+
+
+	$('.product-pic-zoom').zoom();
+
+
+
+})(jQuery);
+
+
+		/*------------------
+		cart
+	--------------------*/
+	var ctx = 
+	document.getElementById("myChart").getContext('2d');
+	var myChart = new Chart(ctx, {
+		type: 'bar',
+		data: {
+			labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+			datasets: [{
+				label: '# of Votes',
+				data: [12, 19, 3, 23, 2, 3],
+				backgroundColor: [
+				'rgba(255, 99, 132, 0.2)',
+				'rgba(54, 162, 235, 0.2)',
+				'rgba(255, 206, 86, 0.2)',
+				'rgba(75, 192, 192, 0.2)',
+				'rgba(153, 102, 255, 0.2)',
+				'rgba(255, 159, 64, 0.2)'
+				],
+				borderColor: [
+				'rgba(255,99,132,1)',
+				'rgba(54, 162, 235, 1)',
+				'rgba(255, 206, 86, 1)',
+				'rgba(75, 192, 192, 1)',
+				'rgba(153, 102, 255, 1)',
+				'rgba(255, 159, 64, 1)'
+				],
+				borderWidth: 1
+			}]
+		},
+		options: {
+			scales: {
+				yAxes: [{
+					ticks: {
+						beginAtZero:true
+					}
+				}]
+			}
+		}
+	});
+
